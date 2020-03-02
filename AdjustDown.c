722c68b7d59685e6c40914bed2a7896aad706aec
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-03-03 00:06:29 +0800

    Update AdjustDown.c

--- AdjustDown.c
+++ AdjustDown.c
@@ -2,12 +2,15 @@
 
 void AdjustDown(int* a, int n, int root)
 {
+   int parent = root;
+   int child = parent*2+1;
    while(child < n)
    {
-      if(child+1 <n && a[child+1] < a[child])
+      if(child+1 < n && a[child+1] < a[child])
       {
          child++;
       }
+      
       if(a[parent] > a[child])
       {
         int tmp = a[parent];
@@ -17,6 +20,7 @@ void AdjustDown(int* a, int n, int root)
         parent = child;
         child = parent*2+1;
       }
+      
       else
       break;
    }
