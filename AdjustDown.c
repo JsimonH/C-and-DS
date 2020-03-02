2f7f039adf04c6898b36820d4c049eca944c457e
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-03-02 23:58:27 +0800

    AdjustDown

---
+++ AdjustDown.c
@@ -0,0 +1,23 @@
+/*向下调整算法*/
+
+void AdjustDown(int* a, int n, int root)
+{
+   while(child < n)
+   {
+      if(child+1 <n && a[child+1] < a[child])
+      {
+         child++;
+      }
+      if(a[parent] > a[child])
+      {
+        int tmp = a[parent];
+        a[parent] = a[child];
+        a[child] = tmp;
+        
+        parent = child;
+        child = parent*2+1;
+      }
+      else
+      break;
+   }
+}
